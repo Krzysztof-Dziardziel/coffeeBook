975560ffe93fefae00722c7cfe4125bcdd551d44
Krzysztof Dziardziel <krzysztof.dziardziel@students.akademeia.edu.pl>
2020-07-27 03:53:57 +0200

    Created CoffeeTool stful as a method to create new tool tiles in settings

--- lib/settings/yourSet.dart
+++ lib/settings/yourSet.dart
@@ -1,4 +1,3 @@
-import 'package:coffeebook/settings/toggleBtn.dart';
 import 'package:flutter/material.dart';
 
 //POSSIBLE ITEMS:
@@ -18,70 +17,65 @@ class YourSet extends StatefulWidget {
 }
 
 class _YourSetState extends State<YourSet> {
-  bool aero = false;
-  bool v60 = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Center(
+            child: Wrap(
+              alignment: WrapAlignment.center,
+              runAlignment: WrapAlignment.center,
+              children: <Widget>[
+                CoffeeTool(name: 'V60'),
+                CoffeeTool(name: 'Aeropress'),
+                CoffeeTool(name: 'Gabi'),
+                CoffeeTool(name: 'Delter'),
+                CoffeeTool(name: 'Chemex'),
+                CoffeeTool(name: 'Frenchpress'),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CoffeeTool extends StatefulWidget {
+  CoffeeTool({@required this.name, this.isAvailable = false});
+  final String name;
+  bool isAvailable;
+  @override
+  _CoffeeToolState createState() => _CoffeeToolState();
+}
+
+class _CoffeeToolState extends State<CoffeeTool> {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
     var btnWidth = width * 0.35;
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  MaterialButton(
-                    child: Container(
-                      height: btnWidth,
-                      width: btnWidth,
-                      child: Text(
-                        'V60',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      alignment: Alignment.center,
-                    ),
-                    color: v60 ? Colors.brown : Colors.white,
-                    onPressed: () {
-                      setState(
-                        () {
-                          v60 = !v60;
-                        },
-                      );
-                    },
-                  ),
-                  MaterialButton(
-                    child: Container(
-                      height: btnWidth,
-                      width: btnWidth,
-                      child: Text(
-                        'AeroPress',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      alignment: Alignment.center,
-                    ),
-                    color: aero ? Colors.brown : Colors.white,
-                    onPressed: () {
-                      setState(
-                        () {
-                          aero = !aero;
-                        },
-                      );
-                    },
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  ToggleBtn(),
-                ],
-              ),
-            ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: MaterialButton(
+        child: Container(
+          height: btnWidth,
+          width: btnWidth,
+          child: Text(
+            widget.name,
+            style: TextStyle(fontSize: 20),
           ),
+          alignment: Alignment.center,
         ),
+        color: widget.isAvailable ? Colors.brown : Colors.white,
+        onPressed: () {
+          setState(
+            () {
+              widget.isAvailable = !widget.isAvailable;
+            },
+          );
+        },
       ),
     );
   }
